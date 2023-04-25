462b1d0b781fc64ea23e0eac44bd80f3903c0217
bigballsb <48416780+bigballsb@users.noreply.github.com>
2023-04-25 22:16:06 +0100

    Delete Python-Rummy-master directory

--- Python-Rummy-master/rummy/constants/constants.py
+++
@@ -1,8 +0,0 @@
-# coding=utf-8
-import sys
-
-
-UNICODE_SUPPORT = sys.stdout.encoding.lower().startswith('utf')
-VALUES = ('A', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K')
-SUITS = ("♠", "♥", "♦", "♣")
-ALPHA_SUITS = ("S", "H", "D", "C")
--- Python-Rummy-master/tests/player/test_ai.py
+++
@@ -1,6 +0,0 @@
-# coding=utf-8
-
-
-class TestAI:
-    # This test suite leaves out functions that primarily serve to render templates/text
-    pass
